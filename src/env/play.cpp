46004c1794a71eda2ea1ead82cdc700e9622db17
adelille <adelille@student.42.fr>
2022-05-14 01:15:08 +0200

    fixes

--- src/env/play.cpp
+++ src/env/play.cpp
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 17:57:17 by adelille          #+#    #+#             */
-/*   Updated: 2022/05/14 01:09:45 by adelille         ###   ########.fr       */
+/*   Updated: 2022/05/14 01:14:38 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	env::_try_word(const char word[WORD_LEN])
 {
-	(void)word;
 	// test if word is valid to find
 	// find word in set
 
 	// inc if found
-	this->_try++;
+	if (word[0] != '4')
+		this->_try++;
 	// else	// show message
 }
 
@@ -48,19 +48,22 @@ void	env::play(void)
 			resize();
 			clear();
 		}
-		else if (key == KEY_ENTER)
+		else if (key == KEY_ENTER || key == 10)
+		{
 			this->_try_word(this->_words_tried[this->_try]);
+			i = 0;
+		}
 		else if ((key == KEY_BACKSPACE || key == 127) && i > 0)
 		{
 			i--;
-			this->_words_tried[this->_try][i] = '\0';
+			//this->_words_tried[this->_try][i] = '\0';
 			move(((this->_row - 3) - WORD_TRY) / 2 + this->_try,
 				(this->_col - WORD_LEN) / 2 + i);
 			addch(' ');
 		}
 		else if (key >= 'a' && key <= 'z' && i < WORD_LEN)
 		{
-			this->_words_tried[this->_try][i] = key - 32;
+			this->_words_tried[this->_try][i] = key;
 			addch(key - 32);
 			i++;
 		}
