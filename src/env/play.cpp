2ebd074559108d6c60429e76c1fbe2ed446fb22a
adelille <adelille@student.42.fr>
2022-05-15 16:02:02 +0200

    fix

--- src/env/play.cpp
+++ src/env/play.cpp
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 17:57:17 by adelille          #+#    #+#             */
-/*   Updated: 2022/05/15 15:37:40 by adelille         ###   ########.fr       */
+/*   Updated: 2022/05/15 16:01:49 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	env::play(void)
 			clear();
 			graphic::frame(this->_row, this->_col);
 		}
-		else if (key == KEY_ENTER || key == 10)
+		else if ((key == KEY_ENTER || key == 10) && i > 0)
 		{
 			this->_words_tried[this->_try][i] = '\0';
 			
